0557ef4db8882f6d8f66c309af2fe3467bf5b00e
DevAndMeck <fermeckler9608@gmail.com>
2024-11-04 20:09:25 -0600

    actualizacion 4.7

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['lucide-react'],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
